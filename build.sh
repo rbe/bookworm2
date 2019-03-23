79c642291239423ed06ab70283d036ffab9106e3
Ralf Bensmann <ralf@art-of-coding.eu>
2019-03-23 14:54:36 +0100

    Build

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PROJECT=~/project/wbh.bookworm
+BRANCH=develop
+
+#        --single-branch --branch develop --depth 1 \
+if [[ ! -d ${PROJECT} ]]
+then
+    git clone \
+        git@bitbucket.org:artofcoding/bookworm2.git \
+        ${PROJECT}
+fi
+
+git pull --autostash \
+    && ./mvnw -P bookworm.hoerbuchkatalog,bookworm.documentation,bookworm.assembly \
+              clean package \
+    && cd docker \
+    && ./docker-build-hoerbuchkatalog.sh 1
+
+exit 0
