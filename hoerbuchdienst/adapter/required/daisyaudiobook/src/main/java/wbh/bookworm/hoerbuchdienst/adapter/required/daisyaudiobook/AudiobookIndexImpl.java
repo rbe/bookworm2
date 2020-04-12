623f92a3f3d81a2e9e7bd73a92d3cce6c780ebc4
Ralf Bensmann <ralf@art-of-coding.eu>
2020-04-12 17:37:30 +0200

    AudiobookIndex aus Repository separieren

---
+++ hoerbuchdienst/adapter/required/daisyaudiobook/src/main/java/wbh/bookworm/hoerbuchdienst/adapter/required/daisyaudiobook/AudiobookIndexImpl.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2011-2020 art of coding UG, https://www.art-of-coding.eu
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package wbh.bookworm.hoerbuchdienst.adapter.required.daisyaudiobook;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.annotation.Property;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import wbh.bookworm.hoerbuchdienst.domain.required.audiobookindex.AudiobookIndex;
+import wbh.bookworm.hoerbuchdienst.domain.required.audiobook.AudiobookMapper;
+import wbh.bookworm.hoerbuchdienst.domain.required.audiobook.AudiobookViews;
+
+@Singleton
+@Slf4j
+@ToString
+public class AudiobookIndexImpl implements AudiobookIndex {
+
+    private final AudiobookMapper audiobookMapper;
+
+    private final ObjectMapper objectMapper;
+
+    private final ElasticsearchClient elasticsearchClient;
+
+    @Property(name = "hoerbuchdienst.repository.uri")
+    private Path streamBase;
+
+    @Inject
+    public AudiobookIndexImpl(final AudiobookMapper audiobookMapper,
+                              final ObjectMapper objectMapper,
+                              final ElasticsearchClient elasticsearchClient) {
+        this.audiobookMapper = audiobookMapper;
+        this.objectMapper = objectMapper;
+        this.elasticsearchClient = elasticsearchClient;
+    }
+
+    @Override
+    public String[] findAll(String keyword) {
+        return Arrays.stream(elasticsearchClient.findAll(keyword))
+                .map(hit -> hit.getSourceAsMap().get("titelnummer").toString())
+                .collect(Collectors.toUnmodifiableList())
+                .toArray(String[]::new);
+    }
+
+    @Override
+    public boolean index() {
+        List<String> audiobooksAsJson = null;
+        try (final Stream<Path> paths = Files.walk(streamBase.resolve("."), 1)
+                .filter(p -> p.endsWith("Kapitel"))) {
+            audiobooksAsJson = paths.peek(p -> log.info("{}", p.getFileName()))
+                    .map(p -> p.getFileName().toString().replace("Kapitel", ""))
+                    .map(audiobookMapper::from)
+                    .map(audiobook -> {
+                        try {
+                            return objectMapper.writerWithView(AudiobookViews.SearchIndex.class)
+                                    .writeValueAsString(audiobook);
+                        } catch (JsonProcessingException e) {
+                            log.error("", e);
+                            return null;
+                        }
+                    })
+                    .filter(Predicate.not("null"::equalsIgnoreCase))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toUnmodifiableList());
+        } catch (IOException e) {
+            throw new ElasticsearchClientException(e);
+        }
+        return elasticsearchClient.bulkIndex(audiobooksAsJson);
+    }
+
+}
---
+++ hoerbuchdienst/domain/required/src/main/java/wbh/bookworm/hoerbuchdienst/domain/required/audiobookindex/AudiobookIndex.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2011-2020 art of coding UG, https://www.art-of-coding.eu
+ * Alle Rechte vorbehalten. Nutzung unterliegt Lizenzbedingungen.
+ * All rights reserved. Use is subject to license terms.
+ */
+
+package wbh.bookworm.hoerbuchdienst.domain.required.audiobookindex;
+
+public interface AudiobookIndex {
+
+    String[] findAll(String keyword);
+
+    boolean index();
+
+}
