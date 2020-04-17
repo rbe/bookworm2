385a06f5b0c69b45f5776b468ac09e8d7c01bed4
Ralf Bensmann <ralf@art-of-coding.eu>
2020-04-17 08:58:59 +0200

    Application und Container

--- hoerbuchdienst/application/impl/src/main/java/wbh/bookworm/hoerbuchdienst/application/impl/Hoerbuchdienst.java
+++ hoerbuchdienst/application/impl/src/main/java/wbh/bookworm/hoerbuchdienst/application/impl/Hoerbuchdienst.java
@@ -4,7 +4,7 @@
  * All rights reserved. Use is subject to license terms.
  */
 
-package wbh.bookworm.hoerbuchdienst.application;
+package wbh.bookworm.hoerbuchdienst.application.impl;
 
 import io.micronaut.runtime.Micronaut;
 
