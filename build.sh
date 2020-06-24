43dc53c8d4cf44d663be95bdc0535f9776058f7e
Ralf Bensmann <ralf@art-of-coding.eu>
2020-06-24 20:20:12 +0200

    Build

--- build.sh
+++ build.sh
@@ -17,34 +17,27 @@ execdir="$(
 hostname="$(hostname -f)"
 MAVEN_REPO="${execdir}/../.m2"
 MAVEN_REPO_CNT="/var/local/.m2"
-MAVEN_OPTS="-Dmaven.repo.local=${MAVEN_REPO_CNT} -Dmaven.artifact.threads=10"
+MAVEN_OPTS="-B -s settings.xml -ff -Dmaven.repo.local=${MAVEN_REPO_CNT} -Dmaven.artifact.threads=10"
 
 echo "Creating local Maven repository destination ${MAVEN_REPO}"
 mkdir -p "${MAVEN_REPO}"
-docker run \
-  --rm \
-  --name maven \
-  --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  --mount type=bind,source=${execdir}/../mikrokosmos,destination=/var/local/mikrokosmos \
-  --mount type=bind,source=${MAVEN_REPO},destination=${MAVEN_REPO_CNT} \
-  maven:3.6.3-openjdk-11 \
-  ls -laR /var/local
 echo "done"
 
 echo "Updating Mikrokosmos"
 pushd "${execdir}"/../mikrokosmos >/dev/null
 git reset --hard
 git pull
+rm .mvn/maven.config
 echo "done"
 echo "Building Mikrokosmos"
 docker run \
   --rm \
   --name maven \
   --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  --mount type=bind,source=${execdir}/../mikrokosmos,destination=/var/local/mikrokosmos \
+  --mount type=bind,source=${execdir}/../mikrokosmos,destination=/var/local/source \
   --mount type=bind,source=${MAVEN_REPO},destination=${MAVEN_REPO_CNT} \
   maven:3.6.3-openjdk-11 \
-  bash -c "cd /var/local/mikrokosmos && mvn clean && mvn compile && mvn package && mvn verify"
+  bash -c "cd /var/local/source && mvn clean && mvn compile && mvn package && mvn verify"
 popd >/dev/null
 echo "done"
 
@@ -52,17 +45,18 @@ echo "Updating WBH Bookworm"
 pushd "${execdir}" >/dev/null
 git reset --hard
 git pull
+rm .mvn/maven.config
 echo "done"
 echo "Building WBH Bookworm"
 docker run \
   --rm \
   --name maven \
   --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  --mount type=bind,source=${execdir},destination=/var/local/bookworm2 \
+  --mount type=bind,source=${execdir},destination=/var/local/source \
   --mount type=bind,source=${MAVEN_REPO},destination=${MAVEN_REPO_CNT} \
   -e MAVEN_OPTS="${MAVEN_OPTS} -Ddomain=${hostname}" \
   maven:3.6.3-openjdk-11 \
-  bash -c "cd /var/local/bookworm2 && mvn clean && mvn compile && mvn package && mvn verify"
+  bash -c "cd /var/local/source && mvn clean && mvn compile && mvn package && mvn verify"
 popd >/dev/null
 echo "done"
 
