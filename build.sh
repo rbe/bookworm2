de45b1227d28096b6450960243a49ba4e28bae30
Ralf Bensmann <ralf@art-of-coding.eu>
2020-06-24 19:34:12 +0200

    Build

--- build.sh
+++ build.sh
@@ -15,19 +15,33 @@ execdir="$(
 )"
 
 hostname="$(hostname -f)"
-MAVEN_OPTS="-Ddomain=${hostname}"
+MAVEN_OPTS="-Dmaven.repo.local=/var/local/.m2 -Dmaven.artifact.threads=1"
+
+pushd "${execdir}"/mikrokosmos >/dev/null
+git reset --hard
+git pull
+docker run \
+  --rm \
+  --name maven \
+  --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
+  --mount type=bind,source=${execdir},destination=/var/local/mikrokosmos \
+  --mount type=bind,source=${execdir}/.m2,destination=/var/local/.m2 \
+  maven:3.6.3-openjdk-11 \
+  bash -c "cd /var/local/mikrokosmos && mvn clean && mvn compile && mvn package && mvn verify"
+popd >/dev/null
 
 pushd "${execdir}" >/dev/null
 git reset --hard
 git pull
 docker run \
   --rm \
-  --hostname maven \
+  --name maven \
   --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-  --mount type=bind,source=${execdir},destination=/var/local \
-  -e MAVEN_OPTS="${MAVEN_OPTS}" \
+  --mount type=bind,source=${execdir},destination=/var/local/bookworm2 \
+  --mount type=bind,source=${execdir}/.m2,destination=/var/local/.m2 \
+  -e MAVEN_OPTS="${MAVEN_OPTS} -Ddomain=${hostname}" \
   maven:3.6.3-openjdk-11 \
-  bash -c "cd /var/local && mvn clean && mvn compile && mvn package && mvn verify"
+  bash -c "cd /var/local/bookworm2 && mvn clean && mvn compile && mvn package && mvn verify"
 popd >/dev/null
 
 exit 0
