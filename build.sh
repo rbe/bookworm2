3cfb116a81e5cc8563679e89c47163ef7e6a3316
Ralf Bensmann <ralf@art-of-coding.eu>
2020-06-24 16:50:12 +0200

    Build

--- build.sh
+++ build.sh
@@ -8,7 +8,11 @@
 set -o nounset
 set -o errexit
 
-execdir="$(pushd "$(dirname "$0")" >/dev/null ; pwd ; popd >/dev/null)"
+execdir="$(
+  pushd "$(dirname "$0")" >/dev/null
+  pwd
+  popd >/dev/null
+)"
 
 hostname="$(hostname -f)"
 MAVEN_OPTS="-Ddomain=${hostname}"
@@ -17,13 +21,13 @@ pushd "${execdir}" >/dev/null
 git reset --hard
 git pull
 docker run \
-    --rm \
-    --hostname maven \
-    --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-    --mount type=bind,source=${execdir},destination=/var/local \
-    -e MAVEN_OPTS="${MAVEN_OPTS}" \
-    maven:3.6.3-openjdk-11 \
-    bash -c "cd /var/local && mvn clean && mvn compile && mvn package && mvn verify"
+  --rm \
+  --hostname maven \
+  --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
+  --mount type=bind,source=${execdir},destination=/var/local \
+  -e MAVEN_OPTS="${MAVEN_OPTS}" \
+  maven:3.6.3-openjdk-11 \
+  bash -c "cd /var/local && mvn clean && mvn compile && mvn package && mvn verify"
 popd >/dev/null
 
 exit 0
