48243c7508a000aa7eef0f90729581b5fca72a05
Ralf Bensmann <ralf@art-of-coding.eu>
2020-06-25 19:47:57 +0200

    Deploy

--- build.sh
+++ build.sh
@@ -8,6 +8,15 @@
 set -o nounset
 set -o errexit
 
+if [[ $# != 2 ]]; then
+  echo "usage: $0 <env> <project>"
+  echo "  env        dev | prod"
+  echo "  project    cms-hbk | hbd"
+fi
+env=$1
+shift
+project=$1
+
 execdir="$(
   pushd "$(dirname "$0")" >/dev/null
   pwd
@@ -22,6 +31,15 @@ MAVEN_REPO="$(
 MAVEN_REPO_CNT="/var/local/maven-repository"
 MAVEN_OPTS="-Xshare:on -XX:TieredStopAtLevel=1 -XX:+UseParallelGC -Dmaven.repo.local=${MAVEN_REPO_CNT} -DlocalRepository=${MAVEN_REPO_CNT} -Dmaven.artifact.threads=10"
 MAVEN_CMD_LINE_ARGS="-B -s .mvn/settings.xml --fail-fast"
+
+case "${project}" in
+cms-hbk)
+  MAVEN_PL="wbh.bookworm.cms.assembly,wbh.bookworm.hoerbuckatalog.deployment"
+  ;;
+hbd)
+  MAVEN_PL="wbh.bookworm.hoerbuckdienst.application.assembly"
+  ;;
+esac
 
 echo "Building Docker Image 'builder'"
 pushd "${execdir}"/builder/openjdk11-maven-docker >/dev/null
@@ -69,7 +87,7 @@ docker run \
   -e MAVEN_OPTS="${MAVEN_OPTS} -Ddomain=${HOSTNAME}" \
   -e MAVEN_CMD_LINE_ARGS="${MAVEN_CMD_LINE_ARGS}" \
   wbh-bookworm/builder:1 \
-  ash -c "cd /var/local/source && rm .mvn/maven.config && java -Xshare:dump && mvn help:effective-pom clean verify" |
+  ash -c "cd /var/local/source && rm .mvn/maven.config && java -Xshare:dump && mvn -pl ${MAVEN_PL} help:effective-pom clean verify" |
   tee build-wbh.bookworm.log
 popd >/dev/null
 echo "done"
