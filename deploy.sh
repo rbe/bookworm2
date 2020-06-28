27cfe3a0f1ab397acfa904f9b5d925581ad8104a
Ralf Bensmann <ralf@art-of-coding.eu>
2020-06-28 10:34:01 +0200

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ set -o nounset
 set -o errexit
 
 if [[ $# != 3 ]]; then
-  echo "usage: $0 <env> <project> <timestamp>"
-  echo "  env        dev | prod"
-  echo "  project    cms-hbk | hbd"
-  echo "  timestamp  yyyy-mm-ddThh-mm"
-  exit 1
+    echo "usage: $0 <env> <project> <timestamp>"
+    echo "  env        dev | prod"
+    echo "  project    cms-hbk | hbd"
+    echo "  timestamp  yyyy-mm-ddThh-mm"
+    exit 1
 fi
 env=$1
 shift
@@ -22,44 +22,55 @@ shift
 timestamp=$1
 
 execdir="$(
-  pushd "$(dirname "$0")" >/dev/null
-  pwd
-  popd >/dev/null
+    pushd "$(dirname "$0")" >/dev/null
+    pwd
+    popd >/dev/null
 )"
 assemblydir="$(
-  pushd "${execdir}/assembly/target/dependency" >/dev/null
-  pwd
-  popd >/dev/null
+    pushd "${execdir}/assembly/target/dependency" >/dev/null
+    pwd
+    popd >/dev/null
 )"
 if [[ ! -d "${execdir}/../releases" ]]; then
-  mkdir "${execdir}/../releases"
+    mkdir "${execdir}/../releases"
 fi
 releasedir="$(
-  pushd "${execdir}/../releases" >/dev/null
-  pwd
-  popd >/dev/null
+    pushd "${execdir}/../releases" >/dev/null
+    pwd
+    popd >/dev/null
 )"
-COMPOSE_PROJECT="${env}-${project}"
-ARTIFACT="${env}-${project}-${timestamp}"
 
-echo "Deploying WBH Bookworm ${env}"
-if [[ ! -f "${releasedir}/${ARTIFACT}" ]]; then
-  unzip "${assemblydir}/${ARTIFACT}.zip" \
-    docker-compose.yml docker-compose.${env}.yml wbh-\*.sh \
-    -d "${releasedir}/${ARTIFACT}"
-else
-  echo "Artifact ${ARTIFACT} already exists"
-fi
+ARTIFACTS=()
+case "${project}" in
+    cms-hbk)
+        ARTIFACTS=("wbh.bookworm.hoerbuchkatalog.deployment" "wbh.bookworm.cms.assembly")
+        ;;
+    hbd)
+        ARTIFACTS=("wbh.bookworm.hoerbuchdienst.application.assembly")
+        ;;
+esac
 
-pushd "${releasedir}/${ARTIFACT}" >/dev/null
-echo "Starting ${env} containers from $(pwd)"
-docker-compose \
-  -p "${COMPOSE_PROJECT}" \
-  -f docker-compose.yml -f docker-compose.${env}.yml \
-  up \
-  -d \
-  --no-build
-popd >/dev/null
-echo "done"
+for artifact in "${!ARTIFACTS[@]}"; do
+    echo "Deploying WBH Bookworm ${env} ${artifact}"
+    file="${assemblydir}/${artifact}-${timestamp}"
+    dir="${releasedir}/${artifact}"
+    if [[ ! -f "${dir}" ]]; then
+        unzip "${file}.zip" \
+            docker-compose.yml docker-compose.${env}.yml wbh-\*.sh \
+            -d "${dir}"
+    else
+        echo "Artifact ${artifact} already exists"
+    fi
+    pushd "${dir}" >/dev/null
+    compose_project="${env}-${artifact}"
+    docker-compose \
+        -p "${compose_project}" \
+        -f docker-compose.yml -f docker-compose.${env}.yml \
+        up \
+        -d \
+        --no-build
+    popd >/dev/null
+    echo "done"
+done
 
 exit 0
