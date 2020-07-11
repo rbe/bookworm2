db02eb8e6b411cedfc6aded29af9516f3ab97578
Ralf Bensmann <ralf@art-of-coding.eu>
2020-07-11 12:08:11 +0200

    Build & deploy

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ set -o nounset
 set -o errexit
 
 if [[ $# != 3 ]]; then
-    echo "usage: $0 <env> <project> <timestamp>"
-    echo "  env        dev | prod"
-    echo "  project    hbk | hbd"
-    echo "  timestamp  yyyy-mm-ddThh-mm-ssZ"
-    exit 1
+  echo "usage: $0 <env> <project> <timestamp>"
+  echo "  env        dev | prod"
+  echo "  project    hbk | hbd"
+  echo "  timestamp  yyyy-mm-ddThh-mm-ssZ"
+  exit 1
 fi
 env=$1
 shift
@@ -22,80 +22,80 @@ shift
 timestamp=$1
 
 execdir="$(
-    pushd "$(dirname "$0")" >/dev/null
-    pwd
-    popd >/dev/null
+  pushd   "$(dirname "$0")" >/dev/null
+  pwd
+  popd   >/dev/null
 )"
 assemblydir="$(
-    pushd "${execdir}/assembly/target/dependency" >/dev/null
-    pwd
-    popd >/dev/null
+  pushd   "${execdir}/assembly/target/dependency" >/dev/null
+  pwd
+  popd   >/dev/null
 )"
 releasedir="${execdir}/../releases"
 [[ ! -d "${releasedir}" ]] && mkdir "${releasedir}"
 releasedir="$(
-    pushd "${execdir}/../releases" >/dev/null
-    pwd
-    popd >/dev/null
+  pushd "${execdir}/../releases" >/dev/null
+  pwd
+  popd >/dev/null
 )"
 project_name="${env}-${project}"
 project_dir="${releasedir}/${project_name}-${timestamp}"
 if [[ ! -d "${project_dir}" ]]; then
-    mkdir "${project_dir}"
+  mkdir "${project_dir}"
 fi
 
 function deploy_artifacts() {
-    local artifacts=("$@")
-    echo "Deploying artifacts ${artifacts[*]}"
-    for artifact in "${artifacts[@]}"; do
-        echo "Deploying WBH Bookworm ${env} ${artifact}"
-        file="${assemblydir}/${artifact}-${timestamp}"
-        if [[ ! -d "${project_dir}/${artifact}" ]]; then
-            unzip "${file}.zip" \
-                docker-compose.yml docker-compose.${env}.yml .env \*.sh \
-                -d "${project_dir}/${artifact}"
-        else
-            echo "Artifact ${artifact} already exists hoerbuchkatalog ${project_dir}/${artifact}"
-        fi
-    done
-    echo "done"
+  local artifacts=("$@")
+  echo "Deploying artifacts ${artifacts[*]}"
+  for artifact in   "${artifacts[@]}"; do
+    echo "Deploying WBH Bookworm ${env} ${artifact}"
+    file="${assemblydir}/${artifact}-${timestamp}"
+    if [[ ! -d "${project_dir}/${artifact}" ]]; then
+      unzip "${file}.zip" \
+        docker-compose.yml docker-compose.${env}.yml .env \*.sh \
+        -d "${project_dir}/${artifact}"
+    else
+      echo "Artifact ${artifact} already exists hoerbuchkatalog ${project_dir}/${artifact}"
+    fi
+  done
+  echo "done"
 }
 
 case "${project}" in
-    hbk)
-        artifacts=("wbh.bookworm.hoerbuchkatalog.deployment" "wbh.bookworm.cms.assembly")
-        deploy_artifacts "${artifacts[@]}"
-        for artifact in "${artifacts[@]}"; do
-            pushd "${project_dir}/${artifact}" >/dev/null
-            echo "Starting ${artifact}"
-            docker-compose \
-                -p "${project_name}" \
-                -f docker-compose.yml -f docker-compose.${env}.yml \
-                up \
-                -d \
-                --no-build
-            popd >/dev/null
-            echo "done"
-        done
-        ;;
-    hbd)
-        deploy_artifacts "wbh.bookworm.hoerbuchdienst.assembly"
-        pushd "${project_dir}/wbh.bookworm.hoerbuchdienst.assembly" >/dev/null
-        chmod +x hbd.sh
-        if [[ $(docker volume ls | grep -c "${env}-minio") == 0 ]]; then
-            echo "Provisioning ${project_name}"
-            ./hbd.sh provision "${env}" "${project_name}"
-            echo "done"
-        else
-            echo "Won't provision ${project_name}, there are volumes present already"
-            echo "Execute $(pwd)/hbd.sh at your own risk"
-        fi
-        popd >/dev/null
-        ;;
-    *)
-        echo "Unknown project: ${project}"
-        exit 1
-        ;;
+  hbk)
+    artifacts=("wbh.bookworm.hoerbuchkatalog.deployment" "wbh.bookworm.cms.assembly")
+    deploy_artifacts "${artifacts[@]}"
+    for artifact in "${artifacts[@]}"; do
+      pushd "${project_dir}/${artifact}" >/dev/null
+      echo "Starting ${artifact}"
+      docker-compose \
+        -p "${project_name}" \
+        -f docker-compose.yml -f docker-compose.${env}.yml \
+        up \
+        -d \
+        --no-build
+      popd >/dev/null
+      echo "done"
+    done
+    ;;
+  hbd)
+    deploy_artifacts "wbh.bookworm.hoerbuchdienst.assembly"
+    pushd "${project_dir}/wbh.bookworm.hoerbuchdienst.assembly" >/dev/null
+    chmod +x hbd.sh
+    if [[ $(docker volume ls | grep -c "${env}-minio") == 0 ]]; then
+      echo "Provisioning ${project_name}"
+      ./hbd.sh provision "${env}" "${project_name}"
+      echo "done"
+    else
+      echo "Won't provision ${project_name}, there are volumes present already"
+      echo "Execute $(pwd)/hbd.sh at your own risk"
+    fi
+    popd >/dev/null
+    ;;
+  *)
+    echo "Unknown project: ${project}"
+    exit 1
+    ;;
 esac
 
 exit 0
--- hoerbuchdienst/assembly/src/main/resources-filtered/hbd.sh
+++ hoerbuchdienst/assembly/src/main/resources-filtered/hbd.sh
@@ -5,16 +5,16 @@ set -o nounset
 set -o errexit
 
 execdir="$(
-  pushd   "$(dirname "$0")" >/dev/null
+  pushd "$(dirname "$0")" >/dev/null
   pwd
-  popd   >/dev/null
+  popd >/dev/null
 )"
 
 usage() {
-  echo   "usage: $0 <destroy | provision | start | stop> <env> <project>"
-  echo   "    env        dev | prod"
-  echo   "    project    hbk | hbd"
-  exit   1
+  echo "usage: $0 <destroy | provision | start | stop> <env> <project>"
+  echo "    env        dev | prod"
+  echo "    project    hbk | hbd"
+  exit 1
 }
 
 [[ $# != 3 ]] && usage
@@ -25,101 +25,100 @@ env=$1
 shift
 project="$1"
 shift
+
+domain_name="${domain}"
+tld="${domain_name/*./}"
 dc="docker-compose -p ${project} -f docker-compose.yml -f docker-compose.${env}.yml"
 case "${mode}" in
   destroy)
-    [[ $# -eq 1     ]] && all="$1" || all="no"
-    pushd     "${execdir}" >/dev/null
-    docker-compose     -p "${project}" down
-    set     +o errexit
-    docker     system prune -f
+    [[ $# -eq 1 ]] && all="$1" || all="no"
+    pushd "${execdir}" >/dev/null
+    docker-compose -p "${project}" down
+    set +o errexit
+    docker system prune -f
     # TODO Daten nicht zerstören! Vorher: Master Key aus Vault sichern!
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_miniodata"
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_miniolocal"
-    docker     volume rm "${project}_mcrootconfig"
-    docker     volume rm "${project}_mclocal"
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_keslocal"
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_vaultconfig"
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_vaultfile"
-    [[ "${all}" == "all"     ]] && docker volume rm "${env}-${project}_vaultlogs"
-    docker     volume rm "${project}_rabbitmqconf"
-    docker     volume rm "${project}_rabbitmqdata"
-    docker     volume rm "${project}_virenscannerdata"
-    [[ "${all}" == "all"     ]] && docker volume rm storage_rproxycerts
-    docker     volume rm "${project}"_rproxyconf
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/rproxy:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/vault:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/kes:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/minio:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/mc:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/rabbitmq:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/virenscanner:${HBD_RELEASE}"
-    [[ "${all}" == "all"     ]] && docker image rm "wbh-${project}/hoerbuchdienst:${HBD_RELEASE}"
-    docker     image prune -f
-    set     -o errexit
-    popd     >/dev/null
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_miniodata"
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_miniolocal"
+    docker volume rm "${project}_mcrootconfig"
+    docker volume rm "${project}_mclocal"
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_keslocal"
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_vaultconfig"
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_vaultfile"
+    [[ "${all}" == "all" ]] && docker volume rm "${env}-${project}_vaultlogs"
+    docker volume rm "${project}_rabbitmqconf"
+    docker volume rm "${project}_rabbitmqdata"
+    docker volume rm "${project}_virenscannerdata"
+    [[ "${all}" == "all" ]] && docker volume rm storage_rproxycerts
+    docker volume rm "${project}"_rproxyconf
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/rproxy:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/vault:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/kes:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/minio:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/mc:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/rabbitmq:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/virenscanner:${HBD_RELEASE}"
+    [[ "${all}" == "all" ]] && docker image rm "wbh-${project}/hoerbuchdienst:${HBD_RELEASE}"
+    docker image prune -f
+    set -o errexit
+    popd >/dev/null
     ;;
   provision)
-    pushd     "${execdir}" >/dev/null
-    echo     "Starting nginx"
-    ${dc}     up -d hbd-rproxy
-    if     [[ ! -f /var/lib/docker/volumes/storage_rproxycerts/_data/is_initialized ]]; then
-      domain_name="${domain}"
-      tld="${domain_name/*./}"
-      [[ "${tld}" == "local"     ]] && wait_for_nginx=5 || wait_for_nginx=60
-      echo     "Waiting ${wait_for_nginx} seconds for TLS certificate generation"
-      sleep     ${wait_for_nginx}
-    fi
-    echo     "done"
-    echo     "Starting Vault"
-    ${dc}     up -d vault
-    sleep     5
-    echo     "done"
-    echo     "Starting MinIO KES"
-    ${dc}     up -d kes
-    sleep     5
-    echo     "done"
-    echo     "Starting MinIO to exchange keys"
-    ${dc}     up -d minio
-    sleep     10
-    echo     "done"
-    echo     "Removing MinIO _OLD keys"
-    rm     -f "${execdir}/.env"
+    pushd "${execdir}" >/dev/null
+    echo "Starting nginx"
+    ${dc} up -d hbd-rproxy
+    [[ "${tld}" == "local" ]] && wait_for_nginx=5 || wait_for_nginx=60
+    echo "Waiting ${wait_for_nginx} seconds for TLS certificate generation"
+    sleep ${wait_for_nginx}
+    echo "done"
+    echo "Starting Vault"
+    ${dc} up -d vault
+    sleep 5
+    echo "done"
+    echo "Starting MinIO KES"
+    ${dc} up -d kes
+    sleep 5
+    echo "done"
+    echo "Starting MinIO to exchange keys"
+    ${dc} up -d minio
+    sleep 10
+    echo "done"
+    echo "Removing MinIO _OLD keys"
+    rm -f "${execdir}/.env"
     #sed -i'' \
     #    -e "/MINIO_ACCESS_KEY_OLD=\(.*\)/d" \
     #    "${execdir}/docker-compose*.yml"
     #sed -i'' \
     #    -e "/MINIO_SECRET_KEY_OLD=\(.*\)/d" \
     #    "${execdir}/docker-compose*.yml"
-    echo     "done"
-    echo     "Enabling MinIO KMS auto encryption"
-    sed     -i'' \
-      -e       "s#MINIO_KMS_AUTO_ENCRYPTION=.*#MINIO_KMS_AUTO_ENCRYPTION=on#" \
+    echo "done"
+    echo "Enabling MinIO KMS auto encryption"
+    sed -i'' \
+      -e "s#MINIO_KMS_AUTO_ENCRYPTION=.*#MINIO_KMS_AUTO_ENCRYPTION=on#" \
       "${execdir}/docker-compose.yml"
-    echo     "done"
-    echo     "Waiting for services to come up"
-    ${dc}     up -d
-    sleep     10
-    echo     "done"
-    echo     "Setting up shard (RabbitMQ, MinIO, reverse proxy)"
-    chmod     +x wbh/*.sh
+    echo "done"
+    echo "Waiting for services to come up"
+    ${dc} up -d
+    sleep 10
+    echo "done"
+    echo "Setting up shard (RabbitMQ, MinIO, reverse proxy)"
+    chmod +x wbh/*.sh
     wbh/provision-rabbitmq.sh
     wbh/provision-minio.sh
-    wbh/provision-rproxy.sh     "${project}" minio hoerbuchdienst
-    echo     "done"
-    echo     "Stopping all containers"
-    ${dc}     down
-    echo     "done"
+    wbh/provision-rproxy.sh "${project}" minio hoerbuchdienst
+    echo "done"
+    echo "Stopping all containers"
+    ${dc} down
+    echo "done"
     ;;
   start)
-    pushd     "${execdir}" >/dev/null
-    ${dc}     up -d
-    popd     >/dev/null
+    pushd "${execdir}" >/dev/null
+    ${dc} up -d
+    popd >/dev/null
     ;;
   stop)
-    pushd     "${execdir}" >/dev/null
-    ${dc}     down
-    popd     >/dev/null
+    pushd "${execdir}" >/dev/null
+    ${dc} down
+    popd >/dev/null
     ;;
   *)
     usage
