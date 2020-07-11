54a9d9c399227f4354446ab50c205ec5e0e17bf3
Ralf Bensmann <ralf@art-of-coding.eu>
2020-07-11 20:54:50 +0200

    Fehlermeldung, wenn MinIO keys fehlen

--- hoerbuchdienst/assembly/src/main/docker/mc/docker-entrypoint.sh
+++ hoerbuchdienst/assembly/src/main/docker/mc/docker-entrypoint.sh
@@ -4,10 +4,12 @@
 if [ -n "${MINIO_ACCESS_KEY_FILE}" ]; then
   MINIO_ACCESS_KEY="$(cat ${MINIO_ACCESS_KEY_FILE})"
   export MINIO_ACCESS_KEY
-fi
-if [ -n "${MINIO_SECRET_KEY_FILE}" ]; then
-  MINIO_SECRET_KEY="$(cat ${MINIO_SECRET_KEY_FILE})"
-  export MINIO_SECRET_KEY
+  if [ -n "${MINIO_SECRET_KEY_FILE}" ]; then
+    MINIO_SECRET_KEY="$(cat ${MINIO_SECRET_KEY_FILE})"
+    export MINIO_SECRET_KEY
+  fi
+else
+  echo "Could not find MinIO access key"
 fi
 
 exec mc "$@"
