1a6c177b74a409dafc37700cd2dc1fc9e45264ba
Ralf Bensmann <ralf@art-of-coding.eu>
2020-08-23 17:19:29 +0200

    Sharding: eingangskorb.sh kann mehrere Hörbücher verschieben

--- hoerbuchdienst/assembly/src/main/docker/mc/eingangskorb.sh
+++ hoerbuchdienst/assembly/src/main/docker/mc/eingangskorb.sh
@@ -4,10 +4,10 @@
 set -o nounset
 set -o errexit
 
-if [[ $# != 2 ]]; then
-  echo "usage: $0 <Titelnummer> <shard>"
-  echo "  Titelnummer    Identifier of audiobook"
+if [[ $# -lt 2 ]]; then
+  echo "usage: $0 <shard> <Titelnummer1> [<TitelnummerN> ...]"
   echo "  shard          mc host alias for shard, see 'mc config host list'"
+  echo "  Titelnummer    Identifier of audiobook"
   echo ""
   echo "  Copies ZIP archive of audiobook <Titelnummer> from bucket 'eingangskorb',"
   echo "  unpacks it and moves files into bucket 'hoerbuchdienst'"
@@ -15,13 +15,37 @@ if [[ $# != 2 ]]; then
   exit 1
 fi
 
-TITELNUMMER="$1"
-shift
+function move() {
+  local titelnummer="$1"
+  local shard="$2"
+  zip="minio/eingangskorb/${titelnummer}.zip"
+  tmpdir="/var/local/mc/${titelnummer}_zip"
+  dir="${titelnummer}Kapitel"
+  dst="${shard}/hoerbuchdienst"
+  if mc stat "${zip}" >/dev/null; then
+    mkdir "${tmpdir}"
+    mc cat "${zip}" | unzip -d "${tmpdir}" -
+    if [[ -d "${tmpdir}/${dir}" ]]; then
+      pushd "${tmpdir}" >/dev/null
+      mc mv --recursive "${dir}" "${dst}"
+      popd >/dev/null
+    else
+      echo "Could not unzip ${zip} into ${dir}"
+      exit 1
+    fi
+    rmdir "${tmpdir}"
+  else
+    echo "${zip} not found"
+    exit 1
+  fi
+}
+
 SHARD="$1"
-pushd /var/local/mc >/dev/null
-mc cat minio/eingangskorb/"${TITELNUMMER}.zip" |
-  unzip -d . - &&
-  mc mv --recursive "${TITELNUMMER}Kapitel" "${SHARD}"/hoerbuchdienst
-popd >/dev/null
+shift
+TITELNUMMERN=("$@")
+
+for t in "${TITELNUMMERN[@]}"; do
+  move "${t}" "${SHARD}"
+done
 
 exit 0
