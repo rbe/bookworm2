65b134c81dd8905bcb2b67ef1f445e79ac6f871d
Ralf Bensmann <ralf@art-of-coding.eu>
2020-09-27 13:03:58 +0200

    CMS und Hörbuchkatalog administrieren

--- cnt.sh
+++ cnt.sh
@@ -35,21 +35,57 @@ releasedir="$(
 running_version="$(docker ps --format "{{.Image}}" --filter "name=${project_name}/*" |
   grep -Eow -e '([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}Z)' |
   uniq)"
-if [[ -z "${running_version}" ]]; then
-  echo "No running version found"
-  exit 1
-fi
-current_project_dir="${releasedir}/${project_name}-${running_version}"
+#if [[ -z "${running_version}" ]]; then
+#  echo "No running version found"
+#  exit 1
+#fi
 
 dc="docker-compose -p ${env}-${project}"
 
 case "${project}" in
+  cms)
+    artifacts=("wbh.bookworm.cms.assembly")
+    running_version="$(docker ps --format "{{.Image}}" --filter "name=${env}-hbk/*" |
+      grep -Eow -e '([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}Z)' |
+      uniq)"
+    if [[ -z "${running_version}" ]]; then
+      echo "No running version found"
+      exit 1
+    fi
+    current_project_dir="${releasedir}/${env}-hbk-${running_version}"
+    dc="docker-compose -p ${env}-hbk"
+    pushd "${current_project_dir}" >/dev/null
+    for artifact in "${artifacts[@]}"; do
+      pushd "${artifact}" >/dev/null
+      echo "Executing for version ${running_version}"
+      ${dc} "$@"
+      popd >/dev/null
+    done
+    popd >/dev/null
+    ;;
   hbk)
-    echo "Not implemented"
-    exit 1
+    if [[ -z "${running_version}" ]]; then
+      echo "No running version found"
+      exit 1
+    fi
+    artifacts=("wbh.bookworm.hoerbuchkatalog.deployment")
+    current_project_dir="${releasedir}/${project_name}-${running_version}"
+    pushd "${current_project_dir}" >/dev/null
+    for artifact in "${artifacts[@]}"; do
+      pushd "${artifact}" >/dev/null
+      echo "Executing for version ${running_version}"
+      ${dc} "$@"
+      popd >/dev/null
+    done
+    popd >/dev/null
     ;;
   hbd)
+    if [[ -z "${running_version}" ]]; then
+      echo "No running version found"
+      exit 1
+    fi
     artifacts=("wbh.bookworm.hoerbuchdienst.assembly")
+    current_project_dir="${releasedir}/${project_name}-${running_version}"
     pushd "${current_project_dir}" >/dev/null
     for artifact in "${artifacts[@]}"; do
       pushd "${artifact}" >/dev/null
