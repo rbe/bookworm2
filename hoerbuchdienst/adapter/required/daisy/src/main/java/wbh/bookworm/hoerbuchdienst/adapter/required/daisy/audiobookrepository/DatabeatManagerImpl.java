9995f2e3e0e9e47a6e6df576bd464e52bba72476
Ralf Bensmann <ralf@art-of-coding.eu>
2020-10-24 16:05:56 +0200

    Logging einzelner ObjectMetaInfos entfernt

--- hoerbuchdienst/adapter/required/daisy/src/main/java/wbh/bookworm/hoerbuchdienst/adapter/required/daisy/audiobookrepository/DatabeatManagerImpl.java
+++ hoerbuchdienst/adapter/required/daisy/src/main/java/wbh/bookworm/hoerbuchdienst/adapter/required/daisy/audiobookrepository/DatabeatManagerImpl.java
@@ -135,7 +135,6 @@ class DatabeatManagerImpl implements DatabeatManager {
                         .reduce(0L, Long::sum);
                 // Attention: use Etag value from MinIO as hash value
                 final Map<Titelnummer, List<ShardObject>> audiobookShardObjects = objectMetaInfos.stream()
-                        .peek(omi -> LOGGER.debug("omi: {}", omi))
                         .filter(objectMetaInfo -> objectMetaInfo.getObjectName().endsWith("DAISY/"))
                         .map(omi -> new ShardObject(omi.getObjectName(), omi.getLength(), omi.getEtag()))
                         .collect(Collectors.groupingBy(shardObject -> fromObjectName(shardObject.getObjectId()),
