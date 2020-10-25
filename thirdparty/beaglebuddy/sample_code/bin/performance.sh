ebea1dcdb83a18a80733bbf8d16fe6d1c2abad51
Ralf Bensmann <ralf@art-of-coding.eu>
2020-10-25 16:43:46 +0100

    ThirdParty gelöscht, Einbundung via Maven

--- thirdparty/beaglebuddy/sample_code/bin/performance.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ksh
-$JAVA_HOME/bin/java -cp ../classes
-../../jar/beaglebuddy_mp3.jar com.beaglebuddy.mp3.sample_code.Performance $1
--- thirdparty/beaglebuddy/sample_code/bin/pictures.cmd
+++
@@ -1,1 +0,0 @@
-%java_home%\bin\java -cp ../classes;../../jar/beaglebuddy_mp3.jar com.beaglebuddy.mp3.sample_code.Pictures
