47f6526d66731c06fd3b6b4a29c0dab6b74e381c
Ralf Bensmann <ralf@art-of-coding.eu>
2020-12-01 20:31:28 +0100

    Spring Web CORS

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/MerklisteRestService.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/MerklisteRestService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ public class MerklisteRestService {
         this.hoerbuchResolver = hoerbuchResolver;
     }
 
+    @CrossOrigin(origins = {"http://localhost:8080"})
     //@PutMapping(value = "{titelnummer}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping(value = "{titelnummer}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> fuegeHinzu(@RequestHeader("X-Bookworm-Mandant") final String xMandant,
