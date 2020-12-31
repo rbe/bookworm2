32be21ec2fb62002267b6792c2267238ba9dbf4f
Ralf Bensmann <ralf@art-of-coding.eu>
2020-12-31 15:27:22 +0100

    Hörbuchkatalog: Belastungen, (Erledigte)Bestellkarten filtern

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/HoererarchivRestService.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/HoererarchivRestService.java
@@ -1,11 +1,19 @@
 package wbh.bookworm.hoerbuchkatalog.webservice.rest;
 
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.TemporalAccessor;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +28,8 @@ import wbh.bookworm.shared.domain.Hoerernummer;
 @RequestMapping("/v1/hoererarchiv")
 public class HoererarchivRestService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HoererarchivRestService.class);
+
     private final CdLieferungService cdLieferungService;
 
     private final HoerbuchResolver hoerbuchResolver;
@@ -30,37 +40,104 @@ public class HoererarchivRestService {
         this.hoerbuchResolver = hoerbuchResolver;
     }
 
-    @GetMapping(value = "/belastungen", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = {
+            "/belastungen",
+            "/belastungen/stichwort/{stichwort}"
+    }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AntwortDTO<List<BelastungAntwortDTO>>> belastungen(@RequestHeader("X-Bookworm-Mandant") final String xMandant,
-                                                                             @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer) {
+                                                                             @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer,
+                                                                             @PathVariable(required = false) final String stichwort) {
         final List<Belastung> belastungen = cdLieferungService.belastungen(new Hoerernummer(xHoerernummer));
         if (belastungen.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         final List<BelastungAntwortDTO> data = hoerbuchResolver.toBelastungenAntwortDTO(belastungen);
-        return ResponseEntity.ok(new AntwortDTO<>(Map.of("count", data.size()), data));
+        final List<BelastungAntwortDTO> filtered;
+        if (null != stichwort && !stichwort.isBlank()) {
+            filtered = data.stream()
+                    .filter(dto -> dto.getAutor().contains(stichwort)
+                            || dto.getSachgebietBezeichnung().contains(stichwort)
+                            || dto.getTitel().contains(stichwort)
+                            || dto.getSprecher1().contains(stichwort))
+                    .collect(Collectors.toUnmodifiableList());
+        } else {
+            filtered = data;
+        }
+        final Map<String, Object> meta = Map.of("count", filtered.size(),
+                "stichwort", stichwort);
+        return ResponseEntity.ok(new AntwortDTO<>(meta, filtered));
     }
 
-    @GetMapping(value = "/bestellkarten", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = {
+            "/bestellkarten",
+            "/bestellkarten/stichwort/{stichwort}"
+    }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AntwortDTO<List<BestellkarteAntwortDTO>>> bestellkarten(@RequestHeader("X-Bookworm-Mandant") final String xMandant,
-                                                                                  @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer) {
+                                                                                  @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer,
+                                                                                  @PathVariable(required = false) final String stichwort) {
         final List<Bestellkarte> bestellkarten = cdLieferungService.bestellkarten(new Hoerernummer(xHoerernummer));
         if (bestellkarten.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         final List<BestellkarteAntwortDTO> data = hoerbuchResolver.toBestellkarteAntwortDTO(bestellkarten);
-        return ResponseEntity.ok(new AntwortDTO<>(Map.of("count", data.size()), data));
+        final List<BestellkarteAntwortDTO> filtered;
+        if (null != stichwort && !stichwort.isBlank()) {
+            filtered = data.stream()
+                    .filter(dto -> dto.getAutor().contains(stichwort)
+                            || dto.getSachgebietBezeichnung().contains(stichwort)
+                            || dto.getTitel().contains(stichwort)
+                            || dto.getSprecher1().contains(stichwort))
+                    .collect(Collectors.toUnmodifiableList());
+        } else {
+            filtered = data;
+        }
+        final Map<String, Object> meta = Map.of("count", filtered.size(),
+                "stichwort", stichwort);
+        return ResponseEntity.ok(new AntwortDTO<>(meta, filtered));
     }
 
-    @GetMapping(value = "/erledigteBestellkarten", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = {
+            "/erledigteBestellkarten",
+            "/erledigteBestellkarten/stichwort/{stichwort}",
+            "/erledigteBestellkarten/startdatum/{startdatum}",
+            "/erledigteBestellkarten/stichwort/{stichwort}/startdatum/{startdatum}",
+    }, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AntwortDTO<List<ErledigteBestellkarteAntwortDTO>>> erledigteBestellkarten(@RequestHeader("X-Bookworm-Mandant") final String xMandant,
-                                                                                                    @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer) {
+                                                                                                    @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer,
+                                                                                                    @PathVariable(required = false) final String stichwort,
+                                                                                                    @PathVariable(required = false) final String startdatum) {
         final List<ErledigteBestellkarte> erledigteBestellkarten = cdLieferungService.erledigteBestellkarten(new Hoerernummer(xHoerernummer));
         if (erledigteBestellkarten.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         final List<ErledigteBestellkarteAntwortDTO> data = hoerbuchResolver.toErledigteBestellkarteAntwortDTO(erledigteBestellkarten);
-        return ResponseEntity.ok(new AntwortDTO<>(Map.of("count", data.size()), data));
+        List<ErledigteBestellkarteAntwortDTO> filtered;
+        if (null != stichwort && !stichwort.isBlank()) {
+            filtered = data.stream()
+                    .filter(dto -> dto.getAutor().contains(stichwort)
+                            || dto.getSachgebietBezeichnung().contains(stichwort)
+                            || dto.getTitel().contains(stichwort)
+                            || dto.getSprecher1().contains(stichwort))
+                    .collect(Collectors.toUnmodifiableList());
+        } else {
+            filtered = data;
+        }
+        if (null != startdatum && !startdatum.isBlank()) {
+            try {
+                final TemporalAccessor datum = DateTimeFormatter.ofPattern("dd.MM.yyyy").parse(startdatum);
+                filtered = filtered.stream()
+                        .filter(dto -> dto.getAusleihdatum().isBefore(ChronoLocalDate.from(datum)))
+                        .collect(Collectors.toUnmodifiableList());
+            } catch (DateTimeParseException e) {
+                LOGGER.warn("Kann Startdatum '{}' nicht parsen", startdatum);
+            }
+        } else {
+            filtered = data;
+        }
+        final Map<String, Object> meta = Map.of("count", filtered.size(),
+                "stichwort", stichwort,
+                "startdatum", startdatum);
+        return ResponseEntity.ok(new AntwortDTO<>(meta, filtered));
     }
 
 }
