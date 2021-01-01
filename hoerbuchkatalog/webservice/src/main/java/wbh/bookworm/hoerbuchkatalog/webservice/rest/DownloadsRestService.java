e321aedf519c27bfdd453c6fb2c9ceb2ad4d6430
Ralf Bensmann <ralf@art-of-coding.eu>
2021-01-01 18:20:17 +0100

    Downloads nach augeliehenAm sortieren

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/DownloadsRestService.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/rest/DownloadsRestService.java
@@ -3,6 +3,7 @@ package wbh.bookworm.hoerbuchkatalog.webservice.rest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -113,30 +114,10 @@ public class DownloadsRestService {
                     && warenkorbService.imCdWarenkorbEnthalten(bestellungSessionId, hoerernummer, titelnummer);
             dto.setImWarenkorb(imWarenkorb);
         });
+        hoerbuchAntwortKurzDTOS.sort(Comparator.comparing(HoerbuchAntwortKurzDTO::getAusgeliehenAm));
         final Map<String, Object> meta = Map.of("anzahlMonat", downloadsService.anzahlAktuellerMonat(hoerernummer),
                 "anzahlHeute", downloadsService.anzahlHeute(hoerernummer));
         return ResponseEntity.ok(new AntwortDTO<>(meta, hoerbuchAntwortKurzDTOS));
-    }
-
-    @GetMapping(value = "/datumab/{datumab}/stichwort/{stichwort}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<AntwortDTO<List<HoerbuchAntwortKurzDTO>>> inhaltGefiltert(@RequestHeader("X-Bookworm-Mandant") final String xMandant,
-                                                                                    @RequestHeader("X-Bookworm-Hoerernummer") final String xHoerernummer,
-                                                                                    @RequestHeader(value = "X-Bookworm-BestellungSessionId", required = false) final String xBestellungSessionId,
-                                                                                    @PathVariable final String datumab,
-                                                                                    @PathVariable final String stichwort) {
-        final Hoerernummer hoerernummer = new Hoerernummer(xHoerernummer);
-        final Downloads downloads = downloadsService.downloadsKopie(hoerernummer);
-        final BestellungSessionId bestellungSessionId = BestellungSessionId.of(xBestellungSessionId);
-        final List<HoerbuchAntwortKurzDTO> hoerbuchAntwortKurzDTOS = hoerbuchResolver.toHoerbuchAntwortKurzDTO(new ArrayList<>(downloads.getTitelnummern().keySet()));
-        final boolean downloadErlaubt = downloadsService.downloadErlaubt(hoerernummer);
-        hoerbuchAntwortKurzDTOS.forEach(dto -> {
-            dto.setDownloadErlaubt(downloadErlaubt);
-            dto.setAlsDownloadGebucht(true);
-            final Titelnummer titelnummer = new Titelnummer(dto.getTitelnummer());
-            dto.setAufDerMerkliste(merklisteService.enthalten(hoerernummer, titelnummer));
-            dto.setImWarenkorb(warenkorbService.imCdWarenkorbEnthalten(bestellungSessionId, hoerernummer, titelnummer));
-        });
-        return ResponseEntity.ok(new AntwortDTO<>(Map.of(), hoerbuchAntwortKurzDTOS));
     }
 
     private String format(final LocalDateTime localDateTime) {
