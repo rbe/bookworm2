f892bb84ac6af9617f93fc36fe24f7c7452b4f14
Ralf Bensmann <ralf@art-of-coding.eu>
2021-02-19 09:12:12 +0100

    Webpack

--- cms/approxy/src/main/resources/static/webpack.config.js
+++ cms/approxy/src/main/resources/static/webpack.config.js
@@ -2,23 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        'wbhonline': './hoerbuchkatalog/js/wbhonline.js',
-        'wbhonlineSuchformular': './hoerbuchkatalog/js/wbhonlineSuchformular.js',
-        /*
-        'wbhonlineA11y': './src/wbhonlineA11y.js',
-        'wbhonlineButtons': './src/wbhonlineButtons.js',
-        'wbhonlineCdBestellung': './src/wbhonlineCdBestellung.js',
-        'wbhonlineDownloads': './src/wbhonlineDownloads.js',
-        'wbhonlineHelper': './src/wbhonlineHelper.js',
-        'wbhonlineHoerprobe': './src/wbhonlineHoerprobe.js',
-        'wbhonlineMerkliste': './src/wbhonlineMerkliste.js',
-        'wbhonlineWarenkorb': './src/wbhonlineWarenkorb.js',
-        */
+        'wbhonline': './hoerbuchkatalog/js/wbhonline.js'
     },
     mode: 'development',
     devtool: 'source-map',
     output: {
-        //filename: 'wbhonline.js',
+        filename: 'wbhonline.js',
         path: path.resolve(__dirname, 'dist-dev'),
     },
     optimization: {
--- cms/approxy/src/main/resources/static/webpack.prod.js
+++ cms/approxy/src/main/resources/static/webpack.prod.js
@@ -3,21 +3,11 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
-        'wbhonline': './hoerbuchkatalog/js/wbhonline.js',
-        'wbhonlineSuchformular': './hoerbuchkatalog/js/wbhonlineSuchformular.js',
-        /*
-        'wbhonlineA11y': './src/wbhonlineA11y.js',
-        'wbhonlineButtons': './src/wbhonlineButtons.js',
-        'wbhonlineCdBestellung': './src/wbhonlineCdBestellung.js',
-        'wbhonlineDownloads': './src/wbhonlineDownloads.js',
-        'wbhonlineHelper': './src/wbhonlineHelper.js',
-        'wbhonlineHoerprobe': './src/wbhonlineHoerprobe.js',
-        'wbhonlineMerkliste': './src/wbhonlineMerkliste.js',
-        'wbhonlineWarenkorb': './src/wbhonlineWarenkorb.js',
-        */
+        'wbhonline': './hoerbuchkatalog/js/wbhonline.js'
     },
     mode: 'production',
     output: {
+        filename: 'wbhonline.js',
         path: path.resolve(__dirname, 'dist-prod'),
     },
     optimization: {
