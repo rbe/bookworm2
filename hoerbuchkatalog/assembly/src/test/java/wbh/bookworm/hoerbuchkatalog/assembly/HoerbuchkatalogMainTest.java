78b2225652c3765731fe5a227f6542a5e4a9d16b
Ralf Bensmann <ralf@art-of-coding.eu>
2021-02-19 09:13:52 +0100

    Test

--- hoerbuchkatalog/assembly/src/test/java/wbh/bookworm/hoerbuchkatalog/assembly/HoerbuchkatalogMainTest.java
+++ hoerbuchkatalog/assembly/src/test/java/wbh/bookworm/hoerbuchkatalog/assembly/HoerbuchkatalogMainTest.java
@@ -6,22 +6,64 @@
 
 package wbh.bookworm.hoerbuchkatalog.assembly;
 
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.user.UserException;
+import com.icegreen.greenmail.util.ServerSetup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 class HoerbuchkatalogMainTest {
+
+    private static ServerSetup serverSetup;
+
+    @RegisterExtension
+    static GreenMailExtension greenMail;
+
+    static {
+        serverSetup = new ServerSetup(8025, "127.0.0.1", "smtp")
+                .setVerbose(true);
+        greenMail = new GreenMailExtension(serverSetup);
+    }
+
+    @BeforeEach
+    void beforeEach() throws UserException {
+        greenMail.getManagers().getUserManager()
+                .createUser("user@example.com", "username", "password");
+    }
+
+    @AfterEach
+    void afterEach() {
+        greenMail.stop();
+    }
 
     @Test
     void shouldStartApp() {
         final ConfigurableApplicationContext applicationContext =
                 SpringApplication.run(HoerbuchkatalogMain.class);
         assertTrue(applicationContext.isRunning());
+    }
+
+    @EnableAutoConfiguration
+    @ComponentScan(basePackages = {
+            "wbh.bookworm.hoerbuchkatalog"
+    })
+    public static class TestConfiguration {
     }
 
 }
