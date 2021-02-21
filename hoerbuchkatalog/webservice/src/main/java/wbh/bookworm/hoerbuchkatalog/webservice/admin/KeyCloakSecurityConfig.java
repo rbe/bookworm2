e5d38e69e6f2e88d13ee5d5f8940a0f6aea2cd7a
Ralf Bensmann <ralf@art-of-coding.eu>
2021-02-21 21:16:56 +0100

    KeyCloak

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/admin/KeyCloakSecurityConfig.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/admin/KeyCloakSecurityConfig.java
@@ -1,9 +1,12 @@
 package wbh.bookworm.hoerbuchkatalog.webservice.admin;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,5 +52,38 @@ public class KeyCloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
         http.csrf().disable();
         //.ignoringAntMatchers("/actuator/**");
     }
+
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    /*
+    @Bean
+    public KeycloakConfigResolver keycloakConfigResolver() {
+        return new KeycloakConfigResolver() {
+
+            private KeycloakDeployment keycloakDeployment;
+
+            @Override
+            public KeycloakDeployment resolve(HttpFacade.Request facade) {
+                if (keycloakDeployment != null) {
+                    return keycloakDeployment;
+                }
+
+                String path = "/keycloak.json";
+                InputStream configInputStream = getClass().getResourceAsStream(path);
+
+                if (configInputStream == null) {
+                    throw new RuntimeException("Could not load Keycloak deployment info: " + path);
+                } else {
+                    keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+                }
+
+                return keycloakDeployment;
+            }
+        };
+    }
+    */
 
 }
