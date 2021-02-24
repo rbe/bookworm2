224442fe27b70b0ec8dac6015ebc4971ea133369
Ralf Bensmann <ralf@art-of-coding.eu>
2021-02-24 10:31:28 +0100

    Hörbuchkatalog CORS

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/security/CorsFilterConfig.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/security/CorsFilterConfig.java
@@ -1,19 +1,30 @@
 package wbh.bookworm.hoerbuchkatalog.webservice.security;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 public class CorsFilterConfig {
 
     @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.applyPermitDefaultValues();
+        config.setAllowedMethods(List.of(HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name()));
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        final FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
+                new CorsFilter(source));
         bean.setOrder(0);
         return bean;
     }
