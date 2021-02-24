90013236a356cd17015a87bbfcc3961d24d909b8
Ralf Bensmann <ralf@art-of-coding.eu>
2021-02-24 08:42:45 +0100

    HTTP OPTIONS erlauben, aufräumen

--- hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/admin/KeyCloakSecurityConfig.java
+++ hoerbuchkatalog/webservice/src/main/java/wbh/bookworm/hoerbuchkatalog/webservice/admin/KeyCloakSecurityConfig.java
@@ -1,17 +1,10 @@
 package wbh.bookworm.hoerbuchkatalog.webservice.admin;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.KeycloakDeployment;
-import org.keycloak.adapters.KeycloakDeploymentBuilder;
-import org.keycloak.adapters.spi.HttpFacade;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootProperties;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,64 +42,14 @@ public class KeyCloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
                 .logoutSuccessUrl("/")
                 .and()
                 .authorizeRequests()
-                .antMatchers("/actuator", "/actuator/**").anonymous()
-                .antMatchers("/sba", "/sba/**").anonymous()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/actuator", "/actuator/**").hasRole("admin")
+                .antMatchers("/sba", "/sba/**").hasRole("admin")
                 .antMatchers("/**/private/**").hasRole("admin")
                 .anyRequest().permitAll()
         ;
         http.csrf().disable();
         //.ignoringAntMatchers("/actuator/**");
     }
-
-    /*
-    @Bean
-    @Primary
-    public KeycloakConfigResolver keycloakConfigResolver(final KeycloakSpringBootProperties properties) {
-        return new MyKeycloakSpringBootConfigResolver(properties);
-    }
-
-    public class MyKeycloakSpringBootConfigResolver extends KeycloakSpringBootConfigResolver {
-
-        private final KeycloakDeployment keycloakDeployment;
-
-        public MyKeycloakSpringBootConfigResolver(final KeycloakSpringBootProperties properties) {
-            keycloakDeployment = KeycloakDeploymentBuilder.build(properties);
-        }
-
-        @Override
-        public KeycloakDeployment resolve(final HttpFacade.Request facade) {
-            return keycloakDeployment;
-        }
-
-    }
-    */
-
-    /*
-    @Bean
-    public KeycloakConfigResolver keycloakConfigResolver() {
-        return new KeycloakConfigResolver() {
-
-            private KeycloakDeployment keycloakDeployment;
-
-            @Override
-            public KeycloakDeployment resolve(HttpFacade.Request facade) {
-                if (keycloakDeployment != null) {
-                    return keycloakDeployment;
-                }
-
-                String path = "/keycloak.json";
-                InputStream configInputStream = getClass().getResourceAsStream(path);
-
-                if (configInputStream == null) {
-                    throw new RuntimeException("Could not load Keycloak deployment info: " + path);
-                } else {
-                    keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
-                }
-
-                return keycloakDeployment;
-            }
-        };
-    }
-    */
 
 }
