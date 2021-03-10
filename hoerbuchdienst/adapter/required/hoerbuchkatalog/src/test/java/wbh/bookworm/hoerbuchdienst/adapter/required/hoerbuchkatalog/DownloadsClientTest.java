d32e05080e1afff51d71f67dc6816c05f22521c3
Ralf Bensmann <ralf@art-of-coding.eu>
2021-03-10 17:23:28 +0100

    Test, Coverage

--- hoerbuchdienst/adapter/required/hoerbuchkatalog/src/test/java/wbh/bookworm/hoerbuchdienst/adapter/required/hoerbuchkatalog/DownloadsClientTest.java
+++ hoerbuchdienst/adapter/required/hoerbuchkatalog/src/test/java/wbh/bookworm/hoerbuchdienst/adapter/required/hoerbuchkatalog/DownloadsClientTest.java
@@ -1,16 +1,13 @@
 package wbh.bookworm.hoerbuchdienst.adapter.required.hoerbuchkatalog;
 
 import javax.inject.Inject;
-import java.net.MalformedURLException;
 
-import io.micronaut.http.client.RxHttpClient;
-import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import wbh.bookworm.hoerbuchdienst.domain.required.hoerbuchkatalog.DownloadsClient;
-
 @MicronautTest
+@Disabled("Hörbuchkatalog muss erreichbar sein")
 class DownloadsClientTest {
 
     @Inject
